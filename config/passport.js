6ba09170c3024e74858e6e6bfd881c9d0962298e
Henry Bondah <bondahdean@gmail.com>
2024-06-18 17:49:18 -0600

    1

--- config/passport.js
+++ config/passport.js
@@ -20,9 +20,10 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: 'https://cse341foodiefinder.onrender.com/auth/google/callback', // Ensure this matches the redirect URI in Google Cloud Console
+      callbackURL: '/auth/google/callback',
     },
     async (accessToken, refreshToken, profile, done) => {
+      console.log('Google strategy called');
       try {
         let user = await User.findOne({ googleId: profile.id });
 
@@ -38,8 +39,10 @@ passport.use(
           await user.save();
         }
 
+        console.log('User authenticated:', user);
         done(null, user);
       } catch (err) {
+        console.error('Error in Google strategy:', err);
         done(err, null);
       }
     }
